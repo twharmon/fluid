59adfc1a7d2863fa30e6ec14470f99b8c963b873
Travis Harmon <harmtrav@gmail.com>
2022-05-19 15:32:22 -0500

    Add readme

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,13 +1,13 @@
 import useTheme from './hooks/useTheme'
 import useThemeState from './hooks/useThemeState'
 import useSetThemeState from './hooks/useSetThemeState'
-import FluidRoot from './theme/context'
+import ThemeProvider from './theme/context'
 import Theme, { createTheme } from './theme/Theme'
 import { darken, lighten, saturate, desaturate } from './theme/colors'
 
 export {
     Theme,
-    FluidRoot,
+    ThemeProvider,
     createTheme,
     useTheme,
     useThemeState,
